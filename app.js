e4c6283dee7d4e48818e0a2fd56971620f59a225
Victor Juarez <juarezv1990@gmail.com>
2022-10-07 22:26:32 -0400

    todo list is working well css formatted now

--- app.js
+++ app.js
@@ -1,59 +1,82 @@
-var input = document.getElementById("item");
-var list = document.getElementById("list");
+let input = document.getElementById("item");
+let list = document.getElementById("list");
 
-
-
-var submit = () => {
-  var div = document.createElement("div");
-  var removeBtn = document.createElement("button");
-  var editBtn = document.createElement("button");
-  var span = document.createElement("span");
+const submit = () => {
+  let div = document.createElement("div");
+  let removeBtn = document.createElement("button");
+  let editBtn = document.createElement("button");
+  let span = document.createElement("span");
+  let btnContainer = document.createElement("div");
+  let radioBtnDone = document.createElement("input");
 
   if (input.value) {
     removeBtn.innerHTML = "Remove";
-    removeBtn.className = "Btn";
-    editBtn.innerHTML = "Edit"
-    editBtn.className = "Btn";
+    removeBtn.className = "btn";
+    editBtn.innerHTML = "Edit";
+    editBtn.className = "btn";
     div.className = "taskContainer";
     span.className = "taskItem";
 
     span.innerHTML = input.value;
     input.value = "";
+
+    radioBtnDone.setAttribute("type", "checkbox");
+
     removeBtn.addEventListener("click", () => {
       list.removeChild(div);
-      console.log(`There is now ${list.childElementCount}`);
     });
 
-    div.append(span);
-    div.appendChild(removeBtn);
-    div.appendChild(editBtn);
+    radioBtnDone.addEventListener("change", () => {
+      checked();
+    });
 
-    editBtn.addEventListener("click", () => {edit(span,editBtn)})
+    editBtn.addEventListener("click", () => {
+      edit(span, editBtn);
+    });
+
+    const checked = () => {
+      if (radioBtnDone.checked) {
+        span.style.textDecoration = "line-through";
+        editBtn.disabled = true;
+      } else {
+        span.style.textDecoration = "none";
+        editBtn.disabled = false;
+      }
+    };
+
+    radioBtnDone.className = "checkDone";
+
+    div.append(radioBtnDone);
+
+    div.append(span);
+
+    btnContainer.className = "btnContainer";
+
+    btnContainer.append(removeBtn);
+    btnContainer.append(editBtn);
+    div.append(btnContainer);
 
     list.append(div);
-    console.log(list.childElementCount);
 
+    const edit = (span, editBtn) => {
+      let editInput = document.createElement("input");
+      let doneEditBtn = document.createElement("button");
 
-    var edit = (span, editBtn) => {
-        var editInput = document.createElement("input");
-        var doneEditBtn = document.createElement("button");
-    
-        doneEditBtn.className = "btn";
-        doneEditBtn.innerHTML = "Done";
-        span.replaceWith(editInput);
-        editBtn.replaceWith(doneEditBtn);
-    
-        doneEditBtn.addEventListener("click", () => {editComplete(editInput,doneEditBtn)});
-        editInput.value = span.innerHTML;
-    }
-    
-    var editComplete = (editInput, doneEditBtn) => {
-        editInput.replaceWith(span);
-        span.innerHTML = editInput.value;
-        doneEditBtn.replaceWith(editBtn);
-    }
+      doneEditBtn.className = "btn";
+      doneEditBtn.innerHTML = "Done";
+      span.replaceWith(editInput);
+      editBtn.replaceWith(doneEditBtn);
 
+      doneEditBtn.addEventListener("click", () => {
+        editComplete(editInput, doneEditBtn);
+      });
+      editInput.value = span.innerHTML;
+    };
 
+    let editComplete = (editInput, doneEditBtn) => {
+      editInput.replaceWith(span);
+      span.innerHTML = editInput.value;
+      doneEditBtn.replaceWith(editBtn);
+    };
   }
 };
-
