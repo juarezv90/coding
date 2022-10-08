02e6a2193455cf2a589aa7bdbc5627e6541cbcfd
Victor <juarezv1990@gmail.com>
2022-10-08 11:40:46 -0400

    Updated more of the JS features

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
-let input = document.getElementById("item");
-let list = document.getElementById("list");
+const input = document.getElementById("item");
+const list = document.getElementById("list");
+const submitButton = document.getElementById("submitButton");
+
+/*Add event listener for user pressing Enter and 
+will call the submit button click event*/
+input.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  if (e.key === "Enter") {
+    submitButton.click();
+  }
+})
 
 const submit = () => {
-  let div = document.createElement("div");
-  let removeBtn = document.createElement("button");
-  let editBtn = document.createElement("button");
-  let span = document.createElement("span");
-  let btnContainer = document.createElement("div");
-  let radioBtnDone = document.createElement("input");
+  const taskContainer = document.createElement("div");
+  const removeBtn = document.createElement("button");
+  const editBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const btnContainer = document.createElement("div");
+  const radioBtnDone = document.createElement("input");
 
+  /* First validates that the user has submitted input else will do nothing. */
   if (input.value) {
+    /* Adds the click event listeners to the remove button
+    assign innerHTML and class name, onclick will remove task */
     removeBtn.innerHTML = "Remove";
     removeBtn.className = "btn";
+    removeBtn.addEventListener("click", () => {
+      list.removeChild(taskContainer);
+    });
+
+    /* Adds the click event listeners to the edit button
+    assign innerHTML and class name, calls edit function*/
     editBtn.innerHTML = "Edit";
     editBtn.className = "btn";
-    div.className = "taskContainer";
-    span.className = "taskItem";
-
-    span.innerHTML = input.value;
-    input.value = "";
-
-    radioBtnDone.setAttribute("type", "checkbox");
-
-    removeBtn.addEventListener("click", () => {
-      list.removeChild(div);
-    });
-
-    radioBtnDone.addEventListener("change", () => {
-      checked();
-    });
-
     editBtn.addEventListener("click", () => {
       edit(span, editBtn);
     });
 
+    span.className = "taskItem";
+    span.innerHTML = input.value;
+    input.value = "";
+
+    /* Set attributes for radio button, then assigned classnames
+    and adds event listener to catch property changes.*/
+    radioBtnDone.setAttribute("type", "checkbox");
+    radioBtnDone.className = "checkDone";
+    radioBtnDone.addEventListener("change", () => {
+      checked();
+    });
+
+    btnContainer.className = "btnContainer";
+    btnContainer.append(removeBtn);
+    btnContainer.append(editBtn);
+
+    /* Disable edit button if task is checked and completed,
+    will re-enable task if unchecked */
     const checked = () => {
       if (radioBtnDone.checked) {
         span.style.textDecoration = "line-through";
@@ -44,39 +65,46 @@ const submit = () => {
       }
     };
 
-    radioBtnDone.className = "checkDone";
-
-    div.append(radioBtnDone);
-
-    div.append(span);
-
-    btnContainer.className = "btnContainer";
-
-    btnContainer.append(removeBtn);
-    btnContainer.append(editBtn);
-    div.append(btnContainer);
-
-    list.append(div);
-
+    /* Converts the task to an input that can be edited and will
+    replace the edit button with a done button for when user is done completing
+    their edit*/
     const edit = (span, editBtn) => {
-      let editInput = document.createElement("input");
-      let doneEditBtn = document.createElement("button");
-
+      const editInput = document.createElement("input");
+      const doneEditBtn = document.createElement("button");
       doneEditBtn.className = "btn";
       doneEditBtn.innerHTML = "Done";
+
       span.replaceWith(editInput);
       editBtn.replaceWith(doneEditBtn);
+
+      /** put focus on input edit to put user directly
+       * into edit input */
+      editInput.focus();
+
+
+      editInput.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+          doneEditBtn.click();
+        }
+      });
 
       doneEditBtn.addEventListener("click", () => {
         editComplete(editInput, doneEditBtn);
       });
+
       editInput.value = span.innerHTML;
     };
 
-    let editComplete = (editInput, doneEditBtn) => {
+    const editComplete = (editInput, doneEditBtn) => {
       editInput.replaceWith(span);
       span.innerHTML = editInput.value;
       doneEditBtn.replaceWith(editBtn);
     };
+
+    taskContainer.className = "taskContainer";
+    taskContainer.append(radioBtnDone);
+    taskContainer.append(span);
+    taskContainer.append(btnContainer);
+    list.append(taskContainer);
   }
 };
